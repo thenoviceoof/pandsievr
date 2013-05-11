f54c7ebb5b566992758edf4016fb5487ebcb7f08
Nathan Hwang <thenoviceoof@gmail.com>
2013-05-11 15:16:38 -0400

    Get a dialog-looking thing on launch

--- gen/com/thenoviceoof/pandsievr/R.java
+++ gen/com/thenoviceoof/pandsievr/R.java
@@ -14,10 +14,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class layout {
-        public static final int pandsievr_appwidget=0x7f030000;
+        public static final int note_dialog=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
         /** 
@@ -41,13 +41,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-    }
-    public static final class xml {
-        public static final int pandsievr_widget_provider=0x7f040000;
+        public static final int AppTheme=0x7f050001;
     }
 }
---
+++ src/com/thenoviceoof/pandsievr/NoteDialog.java
@@ -0,0 +1,12 @@
+package com.thenoviceoof.pandsievr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NoteDialog extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.note_dialog);
+	}
+}
